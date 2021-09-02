3a7d287a4d55a7d85cb5464e2097f4b95291bdee
vinay-osseed <vinay@osseed.com>
2021-09-02 20:40:18 +0530

    Added Q: 7

---
+++ assignment-1/7.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 7->Find next Leap Year */
+
+class year
+{
+    protected $current_year;
+    public function __construct()
+    {
+        $this->current_year = date('Y');
+    }
+    public function nxt_leap_yr()
+    {
+        if ($this->current_year % 4 === 0) {
+            echo "{$this->current_year} it's LEAP year.";
+        }
+        for ($i = 1; $i < 5; $i++) {
+            $yr = date('Y', strtotime('+' . $i . ' year'));
+            if ($yr % 4 === 0) {
+                echo "{$yr} it's LEAP year.";
+                break;
+            }
+        }
+    }
+}
+
+$year = new year();
+$year->nxt_leap_yr();  /* instance of year  */
