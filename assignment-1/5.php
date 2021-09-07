fb5d69f524d686b4159a5006137ed0e7c7502da6
vinay-osseed <vinay@osseed.com>
2021-09-07 22:03:03 +0530

    standards are implemented

--- assignment-1/5.php
+++ assignment-1/5.php
@@ -1,12 +1,28 @@
 <?php
+  /*
+  Standards:
+      ->  Naming Conventions : * beDescriptive * (varName,fncName,clsName,flName) (SQL:*_UPPERCASE*)
+      ->  Case Types         : * camelCase     * (CONST:*_UPPERCASE*)
+      ->  Do Comments        : * Good & Bad    * (Snipppets:['/':'single comment','/*':'block comment'])
+      ->  Consistency        : * beConsistant  * (Comments,Code,Values)
+      ->  Indentation        : * TAB = 4space  *
+      ->  Readability        : * useSpaces     *
+      ->  Indentation        : * TAB = 4space  *
+  */
 
-/* 5->Find the Starting Date of Next Week.*/
+/* VinayGawade: 07-09-2021 ->5.Find the Starting Date of Next Week.<- */
 
-class next_week_date{
+class nxtWeekDate{
+
     public function __construct(){
-        echo "The Next Week Date:-".date('d-m-Y',strtotime('next week'));
+
+        # ->strtotime() used to convert String to Date/Time & later print it in d-m-Y format using date()<-
+        echo "The Next Week Date:- ".date('d-m-Y',strtotime('next week'))."\n";
+
     }
+
 }
 
-new next_week_date();  /* instance of next_week_date  */
+$newObj = new nxtWeekDate();  # ->instance of nxtWeekDate class<-
+
 ?>
