d97ff202e746e1c6a3fb142d3234ba5c85405aaa
vinay-osseed <vinay@osseed.com>
2021-09-07 21:05:24 +0530

    standards are implemented

--- assignment-1/2.php
+++ assignment-1/2.php
@@ -1,20 +1,35 @@
 <?php
+  /*
+  Standards:
+      ->  Naming Conventions : * beDescriptive * (varName,fncName,clsName,flName) (SQL:*_UPPERCASE*)
+      ->  Case Types         : * camelCase     * (CONST:*_UPPERCASE*)
+      ->  Do Comments        : * Good & Bad    * (Snipppets:['/':'single comment','/*':'block comment'])
+      ->  Consistency        : * beConsistant  * (Comments,Code,Values)
+      ->  Indentation        : * TAB = 4space  *
+      ->  Readability        : * useSpaces     *
+      ->  Indentation        : * TAB = 4space  *
+  */
 
-// 2-> Write a Program to replace, (comma) with * 'This,is,training'. Use array/string functions.
+  /* VinayGawade: 07-09-2021 ->2.Write a Program to replace, (comma) with * 'This,is,training'. Use array/string functions.<- */
 
-class replace {
-    public function __construct($that,$withthis,$sentence,$method=1)
-    {
-        if($method===1){
-            echo "By Method-1:-\n".str_replace($that,$withthis,$sentence)."\n";
-        }
-        if($method===2){
-            echo "By Method-2:-\n".implode($withthis,explode($that,$sentence))."\n";
-        }
-        if($method>2){
-            echo "\nInvalid 4th Parameter Please Input 1 or 2 only.\n";
-        }
-    }
-}
-new replace(",","*","This,is,training",2);  /*4rth parameter $method is Optional (default=1) */
-?>
+  class replaceString {
+
+      public function __construct($that,$withThis,$sentence,$method=1){ # ->constructor takes 3 required parameters $ 1 optional<-
+
+          if($method === 1):
+              echo "By Method-1:-\n".str_replace($that,$withThis,$sentence)."\n";
+          endif;
+
+          if($method === 2):
+              echo "By Method-2:-\n".implode($withThis,explode($that,$sentence))."\n";
+          endif;
+
+          if($method > 2):
+              echo "\nInvalid 4th Parameter Please Input 1 or 2 only.\n";
+          endif;
+
+      }
+
+  }
+
+  $newObj = new replaceString(",","*","This,is,training");  # ->4rth parameter $method is Optional (default=1)<-
